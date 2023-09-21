59228630aba8ad644a11443cb0dae9656619066f
Caiyuzhen <caiyuzhen00@gmail.com>
2023-09-22 01:47:03 +0800

    init Actor

--- Source/BaseUE/Actors/MyActor02.cpp
+++ Source/BaseUE/Actors/MyActor02.cpp
@@ -8,17 +8,17 @@ AMyActor02::AMyActor02() {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-    // 🔥对StaticMesh 赋予初始值, 生成实例化的组件
+    // 🔥【👇相当于用 C++ 写一个静态网格】对StaticMesh 赋予初始值, 生成实例化的组件
     StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MyStaticMesh"));// 组件是 Actor 的子物体, 【🔥createDefaultSubobject 表示生成一个默认的子物体】, 【 🔥UStaticMeshComponent 表使用模板组件 】, 【 🔥TEXT("MyStaticMesh") 不是名字!! 而是一个标识】
 }
 
-// Called when the game starts or when spawned
+// 👇游戏生成时调用, Called when the game starts or when spawned
 void AMyActor02::BeginPlay() {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
+// 👇每帧调用, Called every frame
 void AMyActor02::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
 
--- Source/BaseUE/Actors/MyActor02.h
+++ Source/BaseUE/Actors/MyActor02.h
@@ -12,19 +12,19 @@ class BASEUE_API AMyActor02 : public AActor
 	GENERATED_BODY()
 	
 public:
-	// Sets default values for this actor's properties
+	// 👇 用来设置 Actor 的默认值
 	AMyActor02();
 
-    // 👇记得在声明完后都需要在 .cpp 内赋予初始值！！
+    // 记得在声明完后都需要在 .cpp 内赋予初始值！！
     UPROPERTY(VisibleAnywhere, Category="My Actor Components") //UPROPERTY() 表示参与到蓝图的反射系统中, 让蓝图可以编辑, 🔥🔥 组件一般就给 VisibleAnywhere, 让编辑器外也可以看见！！
     UStaticMeshComponent* StaticMesh;//U 开头是继承 Object 类型, 因为 StaticMesh 不是一个基本类型, 所以用指针 *是一个指针
 
 protected:
-	// Called when the game starts or when spawned
+	// 👇 在 Actor 生成时（ Actor 生成为用 spawned, Object 生成为用 Constructor ）
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
+	// 👇每帧都会调用
 	virtual void Tick(float DeltaTime) override;
 
 };
