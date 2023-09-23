49d175a3d9877e4518521b707e811f325c668c55
Caiyuzhen <caiyuzhen00@gmail.com>
2023-09-24 00:06:47 +0800

    Actor02 UPROPERTY

--- Source/BaseUE/Actors/MyActor02.cpp
+++ Source/BaseUE/Actors/MyActor02.cpp
@@ -7,11 +7,16 @@ AMyActor02::AMyActor02() {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-    // 🔥【👇相当于用 C++ 写一个静态网格】对StaticMesh 赋予初始值, 生成实例化的组件
+    // 🔥【👇组件的生成: 相当于用 C++ 写一个静态网格】对StaticMesh 赋予初始值, 生成实例化的组件
     StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MyStaticMesh"));// 组件是 Actor 的子物体, 【🔥createDefaultSubobject 表示生成一个默认的子物体】, 【 🔥UStaticMeshComponent 表使用模板组件 】, 【 🔥TEXT("MyStaticMesh") 不是名字!! 而是一个标识】
+
+    // 👇组件内参数的初始化
     InitLocation = FVector(0.0f); //🔥🔥 设置 Actor 组件的 InitLocation 参数的默认值（位置） FVector(0.0f)
     PlacedLocation = FVector(0.0f); //🔥🔥 设置 Actor 组件的 InitLocation 参数的默认值（位置） FVector(0.0f)
     bGotoInitLocation = false; // 默认留在原初位置
+    WorldOrigin = FVector(0.0f); //记录世界原点的位置
+    TickLocationOffset = FVector(0.0f); //用来记录每一帧的偏移量
+    bShouldMove = false; //是否移动
 }
 
 
@@ -21,8 +26,9 @@ void AMyActor02::BeginPlay() {
 	Super::BeginPlay();
 
     // 🚩🚩 Actor (BeginPlay 出生点)的初始化位置
-    PlacedLocation = GetActorLocation(); //【获取】 Actor 的出生点位置
+    PlacedLocation = GetActorLocation(); //【获取】 Actor 的出生点位置, GetActorLocation 为内置方法
 
+    // 根据【蓝图类的设置】（开关决定是否回到设置的初始点）
     if(bGotoInitLocation) {
         SetActorLocation(InitLocation); //【设置】默认出生点在 0,0,0  //SetActorLocation(FVector(0.0f)); //默认出生点在 0,0,0
     }
@@ -34,5 +40,9 @@ void AMyActor02::BeginPlay() {
 // 👇每帧调用, Called every frame
 void AMyActor02::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
+
+    if(bShouldMove) {
+        AddActorLocalOffset(TickLocationOffset); // AddActorLocationOffset 为内置方法, 会更改物体的位置, 值来自 TickLocationOffset 👈这个值是我们定义的, 可以在实例的 detail 面板中进行输入！！！
+    }
 }
 
--- Source/BaseUE/Actors/MyActor02.h
+++ Source/BaseUE/Actors/MyActor02.h
@@ -16,9 +16,10 @@ class BASEUE_API AMyActor02 : public AActor {
         // 👇 用来设置 Actor 的默认值
         AMyActor02(); // AMyActor02 为 AMyActor02 的构造函数, 在类实例化后
 
+        // UPROPERTY() 为 UE 的宏 ————————————————————
         // 👇【声明一个 Actor 组件】记得在声明完后都需要在 .cpp 内赋予初始值！！ UPROPERTY 为宏定义
         UPROPERTY(VisibleAnywhere, Category="My Actor Components") //UPROPERTY() 表示参与到蓝图的反射系统中, 让蓝图可以编辑, 🔥🔥 组件一般就给 VisibleAnywhere, 让编辑器外也可以看见！！
-        UStaticMeshComponent* StaticMesh;// 👈 设置 Actor 组件为一个静态网格, U 开头是继承 Object 类型, 因为 StaticMesh 不是一个基本类型, 所以用指针 *是一个指针
+        UStaticMeshComponent* StaticMesh;// 👈 设置 Actor 组件为一个静态网格, U 开头是继承 Object 类型, 因为 StaticMesh 不是一个基本类型, 所以用指针 *是一个指针, 🚀 UStaticMeshComponent 指针类型最好不要用 EditAnywhere !!
 
         // 👇【出现在实例 detail 菜单内！！】支持更改 MyActor02 在游戏开始后的位置(自定义默认值) -> 【可以编辑的版本】 -> 默认值为 FVector(0.0f)
         UPROPERTY(EditInstanceOnly, Category="My Actor Properties | Vector(更改 MyActor02 的初始化位置)") // EditInstanceOnly 表示仅仅在实例上可编辑（可以在 UE 中进行编辑(在蓝图属性内不可编辑！), 会显示在 Deatil 面板中）
@@ -31,6 +32,20 @@ class BASEUE_API AMyActor02 : public AActor {
         // 👇 【蓝图类的 default 属性内！】用来判断是否要去到初始位置
         UPROPERTY(EditDefaultsOnly, Category="My Actor Properties | Vector(判断是否需要去到初始位置)")
         bool bGotoInitLocation; //必须以小写 b 开头！
+
+        // 👇 VisibleDefaultOnly 只用来显示初始值, 无法修改
+        UPROPERTY(VisibleDefaultsOnly, Category="My Actor Properties | Vector(测试用-只读)")
+        FVector WorldOrigin; //记录世界原点的位置
+
+        // 👇 到处都可以编辑
+        UPROPERTY(EditAnywhere, Category="My Actor Properties | Vector(测试用-任意可编辑,实例跟原型都会出现,常用!)")
+        FVector TickLocationOffset; //用来记录每一帧的偏移量的向量！
+
+        // 👇 到处都可以编辑
+        UPROPERTY(EditAnywhere, Category="My Actor Properties | Vector(测试用-任意可编辑,实例跟原型都会出现,常用!)")
+        bool bShouldMove; //是否移动 -> 开关, 开启后实例就会一直移动
+
+
 
     protected:
         // 👇 在 Actor 生成时（ Actor 生成为用 spawned, Object 生成为用 Constructor ）
