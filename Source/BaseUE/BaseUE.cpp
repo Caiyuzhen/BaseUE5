2b7f12e8e56c742b20b5c95a2e2f05f307db54f3
Caiyuzhen <caiyuzhen00@gmail.com>
2023-09-24 00:13:46 +0800

    Update

--- Source/BaseUE/BaseUE.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "BaseUE.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BaseUE, "BaseUE" );
-
-DEFINE_LOG_CATEGORY(LogBaseUE)
- 
